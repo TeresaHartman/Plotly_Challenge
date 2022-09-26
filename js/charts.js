402319719145115c8ea0d9a67b16953a788c98f2
Teresa Hartman <teresa2h@users.campus>
2022-09-25 21:01:30 -0500

    Finished bubble chart

--- js/charts.js
+++ js/charts.js
@@ -103,15 +103,42 @@ function buildCharts(sample) {
       title: "Top 10 Bacteria Cultures Found",
       //xaxis: { title: "Title"},
       //yaxis: {title: "Y title"}
-      // margin: {
-      //   l: 100,
-      //   r: 100,
-      //   t: 100,
-      //   b: 100
-      // }
+      
     };
 
     // 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot("bar", barData, barLayout);
+
+    // 1. Create the trace for the bubble chart.
+    var bubbleData = [{
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: 'markers',
+      marker: {
+        size: sample_values,
+        color: otu_ids,
+        colorscale: 'Earth',
+      }
+    }];
+
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      title: 'Bacteria Cultures Per Sample',
+      xaxis: {title: "OTU ID"},
+      hovermode: "closest",
+      margin: {
+        l: 100,
+        r: 100,
+        t: 50,
+        b: 100
+      }
+  
+    };
+
+// 3. Use Plotly to plot the data with the layout.
+Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
   });
 }
+
+
